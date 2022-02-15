c83cc5e72165d86aa4556ba3e9583ad266a36648
TosoxDev <benjamingriller@gmail.com>
2022-02-15 20:18:17 +0100

    style: add tabs

--- src/settings/config.hpp
+++ src/settings/config.hpp
@@ -6,18 +6,18 @@
 namespace config
 {
 	const std::map<std::string, std::string> mCommunities = {
-	{"actor_stalker", "stalker_patch_stalker"},
-	{"actor_bandit", "stalker_patch_bandit"},
-	{"actor_csky", "stalker_patch_csky"},
-	{"actor_dolg", "stalker_patch_dolg"},
-	{"actor_freedom", "stalker_patch_freedom"},
-	{"actor_killer", "stalker_patch_killer"},
-	{"actor_army", "stalker_patch_army"},
-	{"actor_ecolog", "stalker_patch_ecolog"},
-	{"actor_monolith", "stalker_patch_monolith"},
-	{"actor_renegade", "stalker_patch_renegade"},
-	{"actor_isg", "stalker_patch_isg"},
-	{"actor_greh", "stalker_patch_greh"}
+		{"actor_stalker", "stalker_patch_stalker"},
+		{"actor_bandit", "stalker_patch_bandit"},
+		{"actor_csky", "stalker_patch_csky"},
+		{"actor_dolg", "stalker_patch_dolg"},
+		{"actor_freedom", "stalker_patch_freedom"},
+		{"actor_killer", "stalker_patch_killer"},
+		{"actor_army", "stalker_patch_army"},
+		{"actor_ecolog", "stalker_patch_ecolog"},
+		{"actor_monolith", "stalker_patch_monolith"},
+		{"actor_renegade", "stalker_patch_renegade"},
+		{"actor_isg", "stalker_patch_isg"},
+		{"actor_greh", "stalker_patch_greh"}
 	};
 
 	const std::map<std::string, std::string> mLanguage = {
